081d470c77f93b0d06374fff21aa4d18c904a8b3
MarikMMR <55550429+MarikMMR@users.noreply.github.com>
2019-10-18 21:17:50 +0300

    6 lesson

--- src/hello/perem.java
+++ src/hello/perem.java
@@ -1,11 +1,11 @@
 package hello;
 
 public class perem {
+    // примитивные типы данных
     static byte b = 127;
     static short aShort = 8;
     static int it = 99;
     static long l = 6546;
-
 
     static float fl=2.6f;
     static double aDouble = 3.7;
@@ -14,17 +14,27 @@ public class perem {
     boolean bool= true;
     boolean bool2 = false;
     //логический оератор принимает значения только правда и ложь
+
+    // Ссылочые ипы данных имеют какие то свойства, это отдельне классы
+     static String st = "hi, guy";
+     static char c = 'i'; // хранит только один символ
+
     public static void main(String[] args) {  // точка входа в программу
         System.out.println("byte: " + b);
         System.out.println("short: " + aShort);
         System.out.println("int: " + it);
         System.out.println("long: " + l);
+        System.out.println("           ");
 
         System.out.println("float: " + fl);
         System.out.println("double: " + aDouble);
+        System.out.println("            ");
         System.out.println("bool: " + true );
         System.out.println("bool2: " + false);
+        System.out.println("               ");
         // Ctrl+d - дубирование строки
+        System.out.println(st);
+        System.out.println(c);
 
     }
 
